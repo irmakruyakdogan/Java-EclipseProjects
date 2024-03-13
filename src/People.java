635b7764968933f0fbc1260eb34d2e33e6652258
irmakruyakdogan <irmakakdogan2@gmail.com>
2024-03-13 16:49:21 +0300

    Eclipse Projects

---
+++ src/People.java
@@ -0,0 +1,18 @@
+
+public class People {
+	
+	public static int nu_people = 0;
+	
+	public People() {
+		
+		nu_people++;
+	}
+	
+	public static String result() {
+		
+		return "Static Method";
+		
+	}
+	
+
+}
