135a4e267a1350c4807b88af99475853efdb3f14
irmakruyakdogan <irmakakdogan2@gmail.com>
2024-03-25 19:14:44 +0300

    IntelliJ IDEA Projects

--- Classes/src/Main.java
+++ Classes/src/Main.java
@@ -8,5 +8,11 @@ public class Main {
         customerManager.Remove();
         customerManager.Update();
 
+        int[] numbers1 = new int[]{1, 2, 3};
+        int[] numbers2 = new int[]{4, 5, 6};
+        numbers2 = numbers1;
+        numbers1[0] = 10;
+        System.out.println(numbers2[0]);
+
     }
 }
